6476137581b17dd269eff697eac9aa1434d7ed40
Tommy <101204239+WTommyboy@users.noreply.github.com>
2023-01-13 03:01:51 +0800

    Update and rename 計算文件中的指定資料的平均值 to work1.py

---
+++ work1.py
@@ -0,0 +1,16 @@
+fname = input("Enter file name: ")
+fh = open(fname,'r')
+import statistics
+rey = []
+counts = 0
+vv = 0
+for line in fh:
+  line = line.rstrip()
+  if line.startswith("X-DSPAM-Confidence: ") :
+     y = line.replace('X-DSPAM-Confidence: ','')
+     y = float(y)
+     rey.append(y)
+     counts = counts+y
+     vv = vv+1
+     zz = (counts/vv)
+print ('Average spam confidence: ', zz)
